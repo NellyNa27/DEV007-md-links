108f73f8805fcbc6fdc0473716f9b4306994b4df
NellyNa27 <nlly.rro@gmail.com>
2023-07-17 22:25:52 -0600

    Empezando MDLinks

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+
+var _thumb = _interopRequireDefault(require("./thumb.png"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-module.exports = () => {
-  // ...
-};
+//----------AXIOS-----------------
+//const axios = require('axios');
+//const promises = [];
+//promises.push(axios.get('https://www.laboratoria.la'));
+//Promise.all(promises).then((res) => {
+//console.log(res);
+//});
+
+//---------IMPORTAMOS-----------------
+const fs = require("fs");
+const path = require("path");
+
+//----------agragamos rutas---------------
+const ruta = process.argv[2];
+const existeArchivo = fs.existsSync(ruta);
+// primera prueba -console.log(ruta);
+//console.log(fs.existsSync(ruta));
+if (existeArchivo === false) {
+  console.log("ERROOOOOR");
+} else {
+  if (path.isAbsolute(ruta) === false) {
+    console.log(path.resolve(ruta));
+  }
+}
+
+//SABER SI UN ARCHIVO EXISTE EN NODE.JS (MÉTODO FS por ejemplo)
