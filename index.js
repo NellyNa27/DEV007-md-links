ef24d8eb0e721f4b873d591501b5b2c4e0f0d92f
NellyNa27 <nlly.rro@gmail.com>
2023-07-28 13:05:49 -0600

    comenzar con extracción de links

--- index.js
+++ index.js
@@ -7,38 +7,43 @@
 //});
 
 //---------IMPORTAMOS-----------------
-//const fs = require("fs");
+// const fs = require("fs");
 import fs from "fs";
-//const path = require("path");
+// const path = require("path");
 import path from "path";
 import { pathIsAbsolute } from "./functions.js";
 
-//----------agregamos rutas--------------
-//SABER SI UN ARCHIVO EXISTE EN NODE.JS (Módulo FS por ejemplo)
+// ----------agregamos rutas--------------
+// SABER SI UN ARCHIVO EXISTE EN NODE.JS (Módulo FS por ejemplo)
 
 export const mdLinks = (ruta, options) => {
   return new Promise((resolve, reject) => {
     let userPath;
-    //resolve (son callback, son funciones): es cuando se resuelve la promesa-then, reject: es rechazada la promesa-catch
-    // Identifica si la ruta existe.
+    // resolve (son callback, son funciones): es cuando se resuelve la promesa-then, reject: es rechazada la promesa-catch
+    // -----------la ruta existe
     if (fs.existsSync(ruta)) {
+      // resolve es para retornar algo.
       // checar o Convertir a una ruta absoluta.
-      // Convertimos la ruta relativa en absoluta
+      // Convertimos la ruta relativa en absoluta.
       if (!pathIsAbsolute(ruta)) {
         userPath = path.resolve(ruta);
         console.log(userPath);
       } else {
         userPath = ruta;
       }
-      // Es archivo?
+      // ---------Es archivo?
       var stats = fs.statSync(userPath);
-      if (stats.isFile()){
-      console.log("es un archivo?" + stats.isFile());
-      // Es un archivo .md?
-
-
-
-    }
+      if (stats.isFile()) { // Extname
+        console.log("es un archivo?" + stats.isFile());
+        // ---------Es un archivo .md?
+        const file = fs.readFile(userPath, "utf-8", (err, data) => {
+          if (err) {
+            console.log("error: ", err);
+          } else { // comenzanzar con extraccion de links
+            console.log(data);
+          }
+        });
+      }
     } else {
       // Si no existe la ruta rechaza la promesa.
       reject("¡ERROR! La ruta no existe");
