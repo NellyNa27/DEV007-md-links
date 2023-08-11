e1d8d21ad2a11c0ed3f5083b653e1b6eb89483d4
NellyNa27 <nlly.rro@gmail.com>
2023-08-10 22:07:43 -0600

    Test al 92%

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,6 +1,8 @@
 
-import {  routeExist, pathIsAbsolute } from "../functions.js";
+import {  routeExist, pathIsAbsolute, absoluteRoute, pathUser, fileExtname } from "../functions.js";
 
+
+// --------La ruta existe?-------
 describe("routeExists", () => {
     it("is a function", () => {
       expect(typeof routeExist).toBe("function");
@@ -12,69 +14,68 @@ describe("routeExists", () => {
       expect(routeExist("prueba.md")).toBe(true);
     });
     it("return false if doesnt exists", () => {
-      expect(routeExist("PRUEBAA.md")).toBe(false);
+      expect(routeExist("READMEE.md")).toBe(false);
     });
   });
 
+  // --------Función para ver si la ruta es absoluta-------------
 describe("pathIsAbsolute", () => {
     it("is a function", () => {
       expect(typeof pathIsAbsolute).toBe("function");
-      });
     });
     it("return a boolean", () => {
-    expect(typeof pathIsAbsolute("readme.md")).toBe("boolean");
-  });
-  /*
-  it("return false if is not absolute", () => {
-    expect(pathIsAbsolute("testReadme.md")).toBe(false);
-    console.log(absoluteRoute("testReadme.md"));
-  });
-  it("return true if is absolute", () => {
-    expect(pathIsAbsolute(
-        "C:\\Users\\isabe\\OneDrive\\Documentos\\Laboratoria\\MDLINKS\\Isa-mdLinks\\testReadme.md"
-      )
-    ).toBe(true);
+      expect(typeof pathIsAbsolute("README.md")).toBe("boolean");
+    });
+    it("return false if is not absolute", () => {
+      expect(pathIsAbsolute("README.md")).toBe(false);
+    });
+    it("return true if is absolute", () => {
+      expect(pathIsAbsolute(
+      "/Users/nellyna/Desktop/LABORATORIA/DEV007-md-links/prueba.md")).toBe(true);
+    });
   });
 
-describe("pathUser", () => {
-  it("is a function", () => {
-    expect(typeof pathUser).toBe("function");
-  });
-  it("return a boolean", () => {
-    expect(typeof pathUser("testReadme.md")).toBe("boolean");
-  });
-  it("return true if its a file", () => {
-    expect(pathUser("testReadme.md")).toBe(true);
-  });
-});
+  //  -------convierte en absoluta---------------
 describe("absoluteRoute", () => {
-  it("is a function", () => {
-    expect(typeof absoluteRoute).toBe("function");
+    it("is a function", () => {
+      expect(typeof absoluteRoute).toBe("function");
+    });
+    it("return absolute path", () => {
+      expect(typeof absoluteRoute("Readme.md")).toBe("string");
+    });
   });
-  it("return absolute path", () => {
-    expect(typeof absoluteRoute("testReadme.md")).toBe("string");
-  });
-});
-
-it("return absolute path", () => {
-  expect(absoluteRoute("testReadme.md")).toBe(
-    "C:\\Users\\isabe\\OneDrive\\Documentos\\Laboratoria\\MDLINKS\\Isa-mdLinks\\testReadme.md");
+    it("return absolute path", () => {
+      expect(absoluteRoute("README.md")).toBe("/Users/nellyna/Desktop/LABORATORIA/DEV007-md-links/README.md");
     });
 
-describe("fileEx", () => {
-it("is a function", () => {
-  expect(typeof fileEx).toBe("function");
-});
-it("return a string", () => {
-  expect(typeof fileEx("testReadme.md")).toBe("string");
-});
-it("return md extension", () => {
-  expect(fileEx("testReadme.md")).toBe(".md");
-});
-it("return js extension", () => {
-  expect(fileEx("functions.js")).toBe(".js");
-});
-it("return empty string", () => {
-  expect(fileEx("/test")).toBe("");
-});
-});*/
+  //  ----------Es un archivo?---------------
+describe("pathUser", () => {
+    it("is a function", () => {
+      expect(typeof pathUser).toBe("function");
+    });
+    it("return a boolean", () => {
+      expect(typeof pathUser("README.md")).toBe("boolean");
+    });
+    it("return true if its a file", () => {
+      expect(pathUser("README.md")).toBe(true);
+    });
+  });
+
+    //  ---------es archivo .md--------------
+describe("fileExtname", () => {
+    it("is a function", () => {
+      expect(typeof fileExtname).toBe("function");
+    });
+    it("return a string", () => {
+    expect(typeof fileExtname("README.md")).toBe("string");
+    });
+    it("return md extension", () => {
+      expect(fileExtname("README.md")).toBe(".md");
+  });
+    it("return js extension", () => {
+      expect(fileExtname("functions.js")).toBe(".js");
+  });
+    it("return empty string", () => {
+      expect(fileExtname("/test")).toBe("");
+  });
+  });
