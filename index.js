b71049f57f44862eec48aaf661d9f00499d5615e
NellyNa27 <nlly.rro@gmail.com>
2023-08-14 10:11:53 -0600

    Readme y test terminados

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ import chalk from "chalk";
 import axios from "axios";
 
 // ----------agregamos rutas--------------
-// SABER SI UN ARCHIVO EXISTE EN NODE.JS (Módulo FS por ejemplo)
+// SABER SI UN ARCHIVO EXISTE EN NODE.JS (Módulo FS)
 export const mdLinks = (ruta, options) => {
   return new Promise((resolve, reject) => {
     let userPath;
-    // resolve (son callback, son funciones): es cuando se resuelve la promesa-then, reject: es rechazada la promesa-catch
+    // resolve (callback, son funciones): Cuando se resuelve la promesa-then, reject: es rechazada la promesa-catch
     // -----------la ruta existe------------
     if (fs.existsSync(ruta)) {
       // resolve es para retornar algo.--------
